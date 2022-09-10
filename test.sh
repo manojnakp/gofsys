73d890e5abc28e50008fa2ee6beec7a2a1c45599
Manojna <manojnakp2003@gmail.com>
2022-09-10 16:13:33 +0530

    exit code
    configure script to return non-zero exit code

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 cd fsys;
 sh pretest.sh;
 go test -coverprofile=coverage.out -v;
-go tool cover -func=coverage.out;
+status=$?;
 sh posttest.sh;
+if test $status -ne 0
+then
+	exit 1
+fi
+go tool cover -func=coverage.out;
+status=$?;
+if test status -ne 0
+then
+	exit 1
+fi
